8ffedb44b77bf710a4f2a1a34111ef43cfdd995a
雨青 <z4ghjdyx@foxmail.com>
2023-01-14 11:52:55 +0800

    feat: init

---
+++ build.rs
@@ -0,0 +1,6 @@
+// build.rs
+
+fn main() {
+    pkg_config::Config::new().probe("zlib").unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+}
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+// src/lib.rs
+
+use std::os::raw::{c_uint, c_ulong};
+
+extern "C" {
+    pub fn crc32(crc: c_ulong, buf: *const u8, len: c_uint) -> c_ulong;
+}
+
+#[test]
+fn test_crc32() {
+    let s = "hello";
+    unsafe {
+        assert_eq!(crc32(0, s.as_ptr(), s.len() as c_uint), 0x3610a686);
+    }
+}
